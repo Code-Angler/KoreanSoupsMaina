0fe7afaaa2435ad5744ef449f0afd6becdc2c82f
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-06-08 21:10:38 +0900

    edit login page and add review page

--- static/JS/app.js
+++ static/JS/app.js
@@ -1,3 +1,11 @@
 function changeToLogin() {
   window.location.href = "../templates/login.html";
 }
+
+function changeToMain() {
+  window.location.href = "../templates/index.html";
+}
+
+function changeToReview() {
+  window.location.href = "../templates/review.html";
+}
