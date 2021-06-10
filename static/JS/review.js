b4e7c92945c2b9479f0354916e4b42e2e937a786
Code-Angler <xkqkrtk@naver.com>
2021-06-10 18:04:20 +0900

    민기님 파일 merge 한 부분입니다. review.js 에 자바 스크립트 부분이랑 review.html 하단에 비활성화 된 부분입니다.

---
+++ static/JS/review.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+                $("#reviews-box").html("");
+                showReview();
+            });
+
+            function makeReview() {
+                // 화면에 입력돼 있는 제목, 리뷰 내용을 가져옵니다.
+                let title = $('#title').val()
+                let review = $('#soupReview').val()
+                // POST /review에 저장(Create)을 요청한다.
+                $.ajax({
+                    type: "POST",
+                    url: "/review",
+                    data: {title_give:title, review_give:review},
+                    success: function (response) {
+                        alert(response["msg"]);
+                        window.location.reload();
+                    }
+                })
+            }
+
+            function showReview() {
+                $.ajax({
+                    type: "GET",
+                    url: "/review",
+                    data: {},
+                    success: function (response) {
+                        let reviews = response['all_reviews']
+                        for (let i = 0; i < reviews.length; i++) {
+                            let title = reviews[i]['title']
+                            let review = reviews[i]['review']
+
+
+                            let temp_html = `<tr>
+                                               <td>${title}</td>
+                                               <td>${review}</td>
+                                               <td><button onclick="deleteReview( '${title}', '${review}')">삭제</button></td>
+                                           </tr>`
+                            $('#reviews-box').append(temp_html)
+                        }
+                    }
+                    }
+                )
+            }
+
+            function deleteReview(title, review) {
+                            $.ajax({
+                                type: 'POST',
+                                url: '/deleteReview',
+                                data: {title_give: title, review_give: review  },
+                                success: function (response) {
+                                    alert(response['msg']);
+                                    window.location.reload()
+                                    console.log()
+                                }
+                            });
+                        }
